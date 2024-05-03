fabb7ac882fce8f2898b440f3456facfd99829ea
willianjonhy05 <willianjohny05@gmail.com>
2024-05-02 21:29:55 -0300

    Novas digitações

--- projeto/loginJwt/serialiezer.py
+++ projeto/loginJwt/serialiezer.py
@@ -24,5 +24,27 @@ class MyTokenObtainPairSerielizer(TokenObtainPairSerializer):
         return token
     
 class RegisterSerielizer(serializers.Serializer):
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != ["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data["username"], email=validated_data["email"]
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
         
