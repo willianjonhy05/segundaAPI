b57732ce7129502c3a1c7989e39d5b5f5ca8183a
willianjonhy05 <willianjohny05@gmail.com>
2024-05-06 21:21:20 -0300

    Views do Serielizer

--- projeto/loginJwt/views.py
+++ projeto/loginJwt/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import User
+from .serialiezer import MyTokenObtainPairSerielizer, RegisterSerielizer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerielizer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegisterSerielizer
+
+
+@api_view(["GET"])
+def getRoutes(request):
+    routes = ["/api/token/", "/api/register/", "/api/token/refresh/"]
+    return Response(routes)
+
+@api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
+def testEndPoint(request):
+    if request.method == "GET":
+        data = f"Parabéns {request.user}, sua API respondeu a sua solicitação"
+        return Response({"response": data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = "Hello buddy"
+        data = f"Parabéns {request.user}, sua API respondeu ao seu POST request"
+        return Response({"response": data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
+
 
 def index(request):
     return HttpResponse('')
